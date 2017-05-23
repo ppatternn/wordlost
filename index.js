47ea0098b307706405d51a18bb4a1290a09e9217
reflorance <reflorance@gmail.com>
2017-05-23 08:46:15 -0700

    mvp

--- index.js
+++ index.js
@@ -12,34 +12,21 @@ app.set('view engine', 'ejs')
 
 app.get('/word', function(req, res, next){
   lookup = req.query.input_get
+  lookup2 = req.query.input_get2
   request(`http://words.bighugelabs.com/api/2/507ab364c2e364a21c82d0ea6118f4c9/${lookup}/json`,
         function (error, response, body){
           parsed_body = JSON.parse(body)
           var results = new JefNode(parsed_body).filter(function(node) {
               if (node.level == 3) { return node.value }
             })
+          console.log("*******" + lookup + "*****")
+          console.log(parsed_body)
           console.log(results)
-          res.render('word', { title: 'title via router method',
+          res.render('word', { title: 'something like the in-between thesaurus',
                                prevlookup: lookup,
+                               prevlookup2: lookup2,
                                results: { words: results } })
       })
 })
-
-/*
-app.post('/word', function(req, res, next){
-  lookup = req.body.input1
-  request(`http://words.bighugelabs.com/api/2/507ab364c2e364a21c82d0ea6118f4c9/${lookup}/json`,
-        function (error, response, body){
-          parsed_body = JSON.parse(body)
-          var results = new JefNode(parsed_body).filter(function(node) {
-              if (node.level == 3) { return node.value }
-            })
-          console.log(results)
-          res.render('word', { title: 'title via router method',
-                               prevlookup: lookup,
-                               results: { words: results } })
-      })
-})
-*/
 
 app.listen(3000, () => console.log('Server running on port 3000'))
