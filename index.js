7ed1394c89a8b8d8087e9ef46f0ff850949cfa1c
reflorance <reflorance@gmail.com>
2017-05-23 05:20:32 -0700

    figured out how to parse results and display w ejs in list

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-
+const JefNode = require('json-easy-filter').JefNode
+const parseJson = require('parse-json')
+const json = '{\n\t"foo": true,\n}'
 const app = express()
-
 app.use(express.static('public'))
 app.use(bodyParser())
+app.set('view engine', 'ejs')
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
+app.get('/word', function(req, res, next){
+  res.render('word', { title: 'title via router method',
+                       prevlookup: 'none',
+                       results: { words: [] } })
+})
+
+app.post('/word', function(req, res, next){
+  lookup = req.body.input1
+  request(`http://words.bighugelabs.com/api/2/507ab364c2e364a21c82d0ea6118f4c9/${lookup}/json`,
+        function (error, response, body){
+          parsed_body = JSON.parse(body)
+          var results = new JefNode(parsed_body).filter(function(node) {
+              if (node.level == 3) { return node.value }
+            })
+          console.log(results)
+          res.render('word', { title: 'title via router method',
+                               prevlookup: lookup,
+                               results: { words: results } })
+      })
+})
+
+
+/*
 app.get('/word', function(req, res){
   var html = '<form action="/word" method="post">' +
                'word to explore:' +
@@ -36,5 +59,6 @@ app.post('/word', function(req, res){
             '</form>'
   res.send(html)
 });
+*/
 
 app.listen(3000, () => console.log('Server running on port 3000'))
