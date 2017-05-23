5562ca31a13fb9f657569a3842f3b4dc7496b0a2
reflorance <reflorance@gmail.com>
2017-05-23 07:39:55 -0700

    messing with css UGH

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ app.set('view engine', 'ejs')
 
 
 app.get('/word', function(req, res, next){
-  res.render('word', { title: 'title via router method',
-                       prevlookup: 'none',
-                       results: { words: [] } })
+  lookup = req.query.input_get
+  request(`http://words.bighugelabs.com/api/2/507ab364c2e364a21c82d0ea6118f4c9/${lookup}/json`,
+        function (error, response, body){
+          parsed_body = JSON.parse(body)
+          var results = new JefNode(parsed_body).filter(function(node) {
+              if (node.level == 3) { return node.value }
+            })
+          console.log(results)
+          res.render('word', { title: 'title via router method',
+                               prevlookup: lookup,
+                               results: { words: results } })
+      })
 })
 
+/*
 app.post('/word', function(req, res, next){
   lookup = req.body.input1
   request(`http://words.bighugelabs.com/api/2/507ab364c2e364a21c82d0ea6118f4c9/${lookup}/json`,
@@ -30,35 +40,6 @@ app.post('/word', function(req, res, next){
                                results: { words: results } })
       })
 })
-
-
-/*
-app.get('/word', function(req, res){
-  var html = '<form action="/word" method="post">' +
-               'word to explore:' +
-               '<input type="text" name="lookup1" placeholder="..." />' +
-               '<br>' +
-               '<button type="submit">go</button>' +
-             '</form>'
-  res.send(html)
-});
-
-app.post('/word', function(req, res){
-  var lookup = req.body.lookup1
-
-  request(`http://words.bighugelabs.com/api/2/507ab364c2e364a21c82d0ea6118f4c9/${lookup}/json`,
-            function (error, response, body){ console.log(body) }
-  )
-
-  var html = 'so: ' + lookup + '<br>' +
-               '<form action="/word" method="post">' +
-               'word to explore:' +
-               '<input type="text" name="lookup1" placeholder="..." />' +
-               '<br>' +
-               '<button type="submit">go</button>' +
-            '</form>'
-  res.send(html)
-});
 */
 
 app.listen(3000, () => console.log('Server running on port 3000'))
